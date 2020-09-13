55f567eb23ff2a5883aa4bda828ac1a9aed33ff6
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-09-13 18:53:45 -0400

    setup Mongo, Mongoose, Express, Handlebars biolerplate

---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require("./config/db");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+//Instantiate Express
+const app = express();
+
+
+//Morgan Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// Login/ Landing Page
+router.get('/', (req, res) => {
+  res.render('login', {
+    layout: 'login'
+  })
+})
+
+// Dashboard
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+
+
+
+module.exports = router
