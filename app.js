478de3eb4e391cb053037b51e278866734671561
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-09-13 20:48:27 -0400

    implemented google auth

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require('express-session')
 const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
+// Passport config
+require("./config/passport")(passport);
+
 connectDB();
 
-//Instantiate Express
+// Instantiate Express
 const app = express();
 
-
-//Morgan Middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+// Morgan Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-//Static Folder
-app.use(express.static(path.join(__dirname, 'public')))
+// Sessions Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/', require('./routes/index'))
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
